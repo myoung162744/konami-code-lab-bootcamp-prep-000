9d88d6c3c2ba129c62eeea1e96dcb57a2d74507f
Matthew Young <myoung162744@gmail.com>
2018-06-07 23:12:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,8 +15,8 @@ var position = 0
 function init() {
   // your code here
   document.body.addEventListener('keydown', function(e){
-    const key = e
-    if (key === codes[position]) {
+  
+    if (e === codes[position]) {
       position++
       if (position === codes.length) {
         alert('Congratulations')
