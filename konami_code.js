86e86aef9f8417f01c0b5c63385daf53a9529c34
Matthew Young <myoung162744@gmail.com>
2018-06-07 23:11:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ var position = 0
 function init() {
   // your code here
   document.body.addEventListener('keydown', function(e){
-    const key = e.key
+    const key = e
     if (key === codes[position]) {
       position++
       if (position === codes.length) {
