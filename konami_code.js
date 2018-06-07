3d7aea718160cb40d1d2ae5af9c5e636753221da
Matthew Young <myoung162744@gmail.com>
2018-06-07 22:52:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+  addEventListener('keydown',function(e){
+    var key = e.key
+    var requiredKey = code[position]
+    if (key === requiredKey) {
+      position++
+      if (position === code.length) {
+        alert('Congratulations')
+        position = 0
+      }
+      else {
+        position = 0
+      }
+    }
+  })
 }
